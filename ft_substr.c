f7f3ea14c4701f08dc540a6133cb1a67e62052e8
Jodene Hii <jodehii@u90z01s01.42kl.edu.my>
2026-08-12 17:45:13 +0800

    YAY SUBSTR DONE

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jodehii <jodehii@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/11 22:24:22 by jodehii           #+#    #+#             */
-/*   Updated: 2026/08/11 23:01:24 by jodehii          ###   ########.fr       */
+/*   Updated: 2026/08/12 17:44:18 by jodehii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,24 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	int		i;
-	char	*sub;
+	size_t			i;
+	unsigned int	s_len;
+	char			*sub;
 
 	i = 0;
-	sub = (char *)malloc(sizeof(char) * (len + 1));
+	s_len = ft_strlen(s);
+	sub = ft_calloc(len + 1, sizeof(char));
 	if (!sub)
 		return (NULL);
-	while (start)
+	while (i < len && s[start + i] && start < s_len)
 	{
-		sub[start] = (char *)s[start];
-		start++;
+		sub[i] = s[start + i];
+		i++;
 	}
+	return (sub);
 }
+
+// int	main(void)
+// {
+// 	printf("sub str from pineapple : %s\n", ft_substr("pineapple", 4, -2));
+// }
