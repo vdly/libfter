dc9747b8c5547678bdb0b28f60a8a1fcc897179f
Jodene Hii <jodehii@u91z01s02.42kl.edu.my>
2026-08-14 16:05:21 +0800

    slight changes for few functions ++ strjoin YAY

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,15 +6,17 @@
 /*   By: jodehii <jodehii@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 15:32:34 by jodehii           #+#    #+#             */
-/*   Updated: 2026/07/27 16:24:51 by jodehii          ###   ########.fr       */
+/*   Updated: 2026/08/12 18:21:28 by jodehii          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+//TODO NOT USE ASCII NUMBER
+
 int	ft_isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	return (0);
 }
